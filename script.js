0d6902bb77b4b3d50625cbd8fdc00cad03700deb
Shreyansh <149056401+justtshreyansh@users.noreply.github.com>
2025-06-18 02:25:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,7 @@
- if (input === null) return false;  // Handle cancel
-
-  // Convert input to string if not a Date object
-  if (!(input instanceof Date)) {
-    input = String(input);
-  }
-  
-  if (input instanceof Date) {
-    return !isNaN(input.getTime());
-  } else if (typeof input === "string") {
-    const parsedDate = new Date(input);
-    return !isNaN(parsedDate.getTime());
-  }
-  
-  return false;
+var isDate = function (input) {
+  return !isNaN(Date.parse(input));
 };
+
 // Do not change the code below.
 const input = prompt("Enter Date.");
 alert(isDate(input));
